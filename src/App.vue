bcf9cb31dfd431bcb9cf14230bd02e54fb6ca847
Zach Winters <zwintersdeveloper@gmail.com>
2017-12-04 15:41:47 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div id="app">
+    <user-input></user-input>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+import UserInput from './components/UserInput.vue'
+
+export default {
+  components: {
+    UserInput,
+    TodoList
+  }
+}
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,11 @@
+<template>
+  <li>
+    {{ todo.text }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['todo']
+}
+</script>
